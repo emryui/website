d5d66346c4fa8cc3179b55aba5950342e3d28cac
Thomas Taylor <thmsmtylr@gmail.com>
2022-10-19 23:58:17 +1100

    chore: setup html lang attr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="min-h-full">
+    <Html lang="en" className="min-h-full">
       <Head />
       <body className="overflow-hidden bg-[#040404] antialiased">
         <Main />
