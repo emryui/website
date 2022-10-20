0cc8d7cfe60bf71a97d6f97f8fc1e250715c9972
Thomas Taylor <thmsmtylr@gmail.com>
2022-10-20 14:42:56 +1100

    test: dynamic & suspense for more performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
+import dynamic from "next/dynamic";
+// import Layout from "../components/Layout";
+import { Suspense } from "react";
+
+const DynamicLayout = dynamic(() => import("../components/Layout"), {
+  suspense: true,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Suspense fallback={`Loading...`}>
+      <DynamicLayout>
+        <Component {...pageProps} />
+      </DynamicLayout>
+    </Suspense>
   );
 }
 
