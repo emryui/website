46b97274974ab3f5211715d71cb1443468b12d7f
Thomas Taylor <thmsmtylr@gmail.com>
2022-10-22 15:36:20 +1100

    feat: add social icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { GitHub, Twitter } from "react-feather";
 
 export const Header = () => (
-  <header className="container relative z-50 mx-auto flex h-16 items-center gap-x-10 p-6 sm:h-20 sm:p-0">
+  <header className="container relative z-50 mx-auto flex h-16 items-center justify-between gap-x-10 p-6 sm:h-20 sm:p-0">
     <Link href="/">
       <a className="inline-flex items-center gap-x-1 text-lg font-bold text-white">
         <Image
@@ -16,6 +17,24 @@ export const Header = () => (
         Emry UI
       </a>
     </Link>
+    <div className="flex items-center gap-x-3">
+      <Link href="https://github.com/emryui">
+        <a
+          target="_blank"
+          className="text-gray-400 transition-colors duration-200 hover:text-primary"
+        >
+          <GitHub className="h-5 w-5" />
+        </a>
+      </Link>
+      <Link href="https://twitter.com/emry_ui">
+        <a
+          target="_blank"
+          className="text-gray-400 transition-colors duration-200 hover:text-primary"
+        >
+          <Twitter className="h-5 w-5" />
+        </a>
+      </Link>
+    </div>
   </header>
 );
 
